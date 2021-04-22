4231d386eb5dfd5a165ebcd9c0edc282719346da
ygorthiago <ygorthiagoff@outlook.com>
2021-04-22 13:48:08 -0300

    refactor: unreachable code removal

--- src/utils/translateStatus.ts
+++ src/utils/translateStatus.ts
@@ -2,21 +2,17 @@ function translateStatus(status: string) {
   switch (status) {
     case 'Released':
       return 'Lançado';
-      break;
     case 'Planned':
       return 'Planejado';
-      break;
     case 'In Production':
       return 'Em produção';
-      break;
     case 'Rumored':
       return 'Rumores';
-      break;
     case 'Canceled':
       return 'Cancelado';
-      break;
     case 'Post Production':
       return 'Pós Produção';
+    default:
       break;
   }
 }
